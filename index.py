db766ebc1d6591f985c8c0bc2929d0f7887ebda5
Bartosz <b.lodek@softwarepartner.pl>
2021-01-19 20:43:39 +0100

    implement fitnessSharing

--- index.py
+++ index.py
@@ -6,20 +6,20 @@ from random import choice
 import functools
 import operator
 import matplotlib.pyplot as plt
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, current_process
 
 def createOptimum(Xopt, K=1):
     return  lambda X : K*e**functools.reduce(operator.add, [-((X[:,i] - x)**2) for i, x in enumerate(Xopt)])
 
 def createTarget():
-    firstOptimum = createOptimum([2, 4])
+    firstOptimum = createOptimum([2, 2])
     secondOptimum= createOptimum([4.2, 2], 2)
     return lambda X: firstOptimum(X) + secondOptimum(X)
 
 def ES1plus1(X, sigma):
     return X + sigma*np.random.normal(0, 1, size=X.shape)
 
-def BEST(X, F):
+def BEST(X, F, target):
     children = np.zeros(shape=X.shape)
     bestIndex = target(X).argmax()
     best = X[bestIndex]
@@ -41,7 +41,7 @@ def roulette(probability):
         if targetValue < sum:
             return index
 
-def proportionalSelection(population):
+def rouletteSelection(population, target):
     result = np.zeros(shape=population.shape)
     quality = target(population)
     sum = quality.sum()
@@ -51,45 +51,76 @@ def proportionalSelection(population):
         result[i] = population[selectedParentIndex]
     return result
 
-def run(q, populationSize, genotypes, startPoint, randRange, F, sigma):
+def f(d, alpha, delta):
+    if d<delta:
+        return 1-((d/delta)**alpha)
+    else:
+         return 0
+
+def fitnessSharing(population, target, alpha, delta):
+    F = target(population)
+    return F/[np.sum([f(np.linalg.norm(Y-X), alpha, delta) for Y in population]) for X in population]
+
+def fitnessSharingForTests(x, population, target, alpha, delta):
+    F = target(x)
+    return F/[np.sum([f(np.linalg.norm(Y-X), alpha, delta) for Y in population]) for X in x]
+
+def run(q, nativeTarget, populationSize, genotypes, startPoint, randRange, F, sigma):
+    np.random.seed(current_process().pid)
+    fig, ax = plt.subplots()
+    target = lambda pop: fitnessSharing(pop, nativeTarget, 0.5, 3)
+#     target = nativeTarget
     population = startPoint + ((np.random.rand(populationSize, genotypes)*(2*randRange))-randRange)
+    fitnesses = []
     steps = 0
     while True:
-        max = target(population).max()
+        fitness = nativeTarget(population)
+        fitnesses.append(fitness.mean())
+        max = nativeTarget(population).max()
         val1 = "{:.2f}".format(targetValue1 - max)
         val2 = "{:.2f}".format(targetValue2 - max)
-        if targetValue2 - max < 0.01:
+        if max > targetValue1*0.1:
             return q.put(steps)
-        elif steps >= 200:
+        if steps >= 400:
             return q.put(steps)
         steps += 1
-#         print(val1, val2)
-        population = proportionalSelection(population)
+        population = BEST(population, F, target)
+        population = rouletteSelection(population, target)
         population = ES1plus1(population, sigma)
-        population = BEST(population, F)
+#         ax.clear()
+#         xTest = np.array([[i, 2] for i in np.arange(population[:, 0].min() - 2, population[:, 0].max() + 2, 2**-4)])
+#         x = np.array([[i, 2] for i in np.arange(0, 6, 2**-4)])
+#         changeFitness = lambda x: fitnessSharingForTests(x, population, nativeTarget, 0.5, 3)
+#         ax.plot(x[:, 0], nativeTarget(x))
+#         ax.plot(xTest[:, 0], changeFitness(xTest))
+#         ax.scatter(population[:, 0], nativeTarget(population), marker='.')
+#         plt.pause(1)
+#     plt.plot(fitnesses)
+#     plt.show()
 
 populationSize = 10
 genotypes = 2
 startPoint = [0, 0]
 randRange = 0.3
-sigma = 0.2
+
+sigma = 0.5
 F = 0.4
 
-target = createTarget()
-targetValue1, targetValue2 = target(np.array([[2, 4], [4.2, 2]]))
-
-attempts = 1000
-sigmas = np.arange(0.1, 0.7, 0.05).tolist()
+attempts = 100
+sigmas = np.arange(0.1, 1, 0.05).tolist()
 times = []
 
-# print(run(populationSize, genotypes, startPoint, randRange, F, sigma))
+customTarget = createTarget()
+targetValue1, targetValue2 = customTarget(np.array([[2, 4], [4.2, 2]]))
 
+# q = Queue()
+# run(q, customTarget, populationSize, genotypes, startPoint, randRange, F, sigma)
 for xindex, sigma in enumerate(sigmas):
     sum = 0
     processes = []
     q = Queue()
     for i in range(attempts):
-        p = Process(target=run, args=(q, populationSize, genotypes, startPoint, randRange, F, sigma))
+        p = Process(target=run, args=(q, customTarget, populationSize, genotypes, startPoint, randRange, F, sigma))
         p.start()
         processes.append(p)
 
@@ -98,7 +129,9 @@ for xindex, sigma in enumerate(sigmas):
         sum+=q.get()
     print(xindex)
     times.append(sum/attempts)
-#     plt.scatter(population, target(population), marker='.')
-#     plt.pause(0.1)
 
+# plt.plot(sigmas, times)
+# plt.xlabel('sigma')
+# plt.ylabel('steps')
+# plt.show()
 print(times)
